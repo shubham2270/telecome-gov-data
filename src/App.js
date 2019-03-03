7faae99f9dd1c945654c453c9d529994c5094621
Shubham Kumar <shubham2270@gmail.com>
2019-03-03 14:47:19 +0530

    Passed data with props

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import './App.css';
 
 class App extends Component {
   render() {
+     const landline_vs_Mobile = 'https://api.data.gov.in/resource/a176cfe8-2f69-4c0a-960d-bb3b16fcb638?api-key=579b464db66ec23bdd000001125b99b03397408164eb5379766559eb&format=json&offset=0';
 
-    // let { isLoaded, items } = this.state;
-
-    // if (!isLoaded) {
-    //   return <div>Loading...</div>
-    // } else {}
+   
       return (
         <div className="App">
           <Header />
-          <Datatable/>
+          <Datatable 
+            year={'Year'} 
+            wired={'Wired'} 
+            wireless={'Wireless'} 
+            apiUrl={landline_vs_Mobile}
+            month={'Apr - Mar'}
+            lakhs={'(In Lakhs)'}
+            />
         </div>
       );
     }
--- src/components/datatable/Datatable.js
+++ src/components/datatable/Datatable.js
@@ -6,51 +6,52 @@ class Datatable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
+      telecomeData: [],
       isLoaded: false,
     }
   }
 
   componentDidMount() {
-    let APIUrl = 'https://api.data.gov.in/resource/a176cfe8-2f69-4c0a-960d-bb3b16fcb638?api-key=579b464db66ec23bdd000001125b99b03397408164eb5379766559eb&format=json&offset=0';
-
-    // let APIUrl = 'https://jsonplaceholder.typicode.com/users';
+    let APIUrl = this.props.apiUrl;
 
     fetch(APIUrl)
     .then(res => res.json())
     .then(data => {
       this.setState({
         isLoaded: true,
-        items: data.records,
-        // items: data,
+        telecomeData: data.records,
       })
     })
     
   }
-  
+
   render() {
+
+    const { year, month, wired, wireless, lakhs } = this.props;
+    const { telecomeData } = this.state;
+
       return (
         <div>
           <div className='wrapper'>
-                  <div className='card'>
-                    <div className='dataTitleWrapper'>
-                    <div className='item year'>Year</div>
-                    <div className='inLakhs'>(Apr - Mar)</div>
+              <div className='card'>
+                  <div className='data_head_Wrapper'>
+                    <div className='data_main_head'>{year}</div>
+                    <div className='data_sub_head'>{month}</div>
                   </div>
                     
-                   <div className='dataTitleWrapper'>
-                      <div className='item'>Wired</div>
-                      <div className='inLakhs'>(In Lakhs)</div>
+                   <div className='data_head_Wrapper'>
+                      <div className='data_main_head'>{wired}</div>
+                      <div className='data_sub_head'>{lakhs}</div>
                    </div>
   
-                    <div className='dataTitleWrapper'>
-                      <div className='item'>Wireless</div>
-                      <div className='inLakhs'>(In Lakhs)</div>
+                    <div className='data_head_Wrapper'>
+                      <div className='data_main_head'>{wireless}</div>
+                      <div className='data_sub_head'>{lakhs}</div>
                    </div>
         
                   
                    {!this.state.isLoaded? 
-                      <>
+                      <> 
                         <div className='data-column'></div>
                         <div className='data-column'>
                         <img src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/fancybox_loading.gif" alt=""></img></div>
@@ -58,7 +59,7 @@ class Datatable extends Component {
                       </> : 
                    <>
                     <div className='data-column'>
-                            {this.state.items.filter(object => (
+                            {telecomeData.filter(object => (
                             object.month === 'December'
                             )).map((item) => {
                               return <div key={item._year_ending_31st_march}>{item._year_ending_31st_march}</div>
@@ -67,7 +68,7 @@ class Datatable extends Component {
                           </div>
                             {/* Wired Data */ }
                           <div className='data-column'>
-                          {this.state.items.filter(object => (
+                          {telecomeData.filter(object => (
                             object.month === 'December'
                             )).map((item) => {
                               return <div key={item._year_ending_31st_march}>{Math.round(item.telephones___wireline/100000)}</div>
@@ -76,25 +77,18 @@ class Datatable extends Component {
                     
                           {/*  Wireless Data*/}
                           <div className='data-column'>
-                          {this.state.items.filter(object => (
+                          {telecomeData.filter(object => (
                             object.month === 'December'
                             )).map((item) => {
                               return <div key={item._year_ending_31st_march}>{Math.round(item.telephones___wireless/100000)}</div>
                             }) }
                       </div>
                     </> 
-                  
                     }
-                  
-                    
                 </div>
-             
           </div>
         </div>
       )
-    
-
-   
   }
 }
 
