e64216561bdcae32c4bb5f728280d46e45b113d9
Shubham Kumar <shubham2270@gmail.com>
2019-03-10 10:52:36 +0530

    Updates

---
+++ precache-manifest.71552cf1b7386941cfd5c9eeb52031b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "979a9a44d2d79d392918",
+    "url": "/telecome-gov-data/static/js/runtime~main.979a9a44.js"
+  },
+  {
+    "revision": "b0bb3210fd41c4183837",
+    "url": "/telecome-gov-data/static/js/main.b0bb3210.chunk.js"
+  },
+  {
+    "revision": "5fbb210b395c0f710db4",
+    "url": "/telecome-gov-data/static/js/2.5fbb210b.chunk.js"
+  },
+  {
+    "revision": "b0bb3210fd41c4183837",
+    "url": "/telecome-gov-data/static/css/main.ecb4ab42.chunk.css"
+  },
+  {
+    "revision": "4f7bfc07f99bbd380eca779a514e9dff",
+    "url": "/telecome-gov-data/index.html"
+  }
+];
